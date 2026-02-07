468ee1b94657ce53205af6a557aeb53a5c90d6e5
JR0619 <2961387R@student.ac.uk>
2026-02-07 22:36:47 +0000

    Chapter6 commit

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,9 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    path('', views.index, name ='index'),
-    path('about/', views.about, name ='about')
-    ]
+  path('', views.index, name='index'),
+  path('about/', views.about, name='about'),
+  path('category/<slug:category_name_slug>/',views.show_category, name='show_category'),
+  path('goto/<int:page_id>/', views.goto_page, name='goto'),
+
+]
